7e4df79ae1b3b1de8c540b119f57cbbfeb6e3b73
KenWorks77 <ken.yokochi.77@gmail.com>
2023-09-29 15:07:41 +0900

    About Us テキスト修正。

--- page-about.php
+++ page-about.php
@@ -6,8 +6,8 @@
     <div>
       <h2 class="fade-in">Ken</h2>
       <P class="fade-in">100% Japanese living in Tokyo.</br>
-<a href="<?php echo esc_url(home_url('travel')); ?>">A world traveler</a> - Love to visit my favorite countries. I have been in France (more than 5 times), GB, Germany, Belgium, Swiss, Italy, Spain, Andorra, New Zealand, and US (more than 10 times).</br>
-<a href="<?php echo esc_url(home_url('aviation')); ?>">An aviation fan</a> - I have attended <a href="https://airrace.org/" target="_blank">Reno Air Races</a> every September since 2008 and have attended <a href="http://www.ajbs.fr/" target="_blank">Meeting Aerien de La Ferté-Alais</a> in France twice.</br>
+<a href="<?php echo esc_url(home_url('travel')); ?>">A world traveler</a> - Love to visit my favorite countries. I visited France (more than 5 times), GB, Germany, Belgium, Swiss, Italy, Spain, Andorra, New Zealand, and US (more than 10 times).</br>
+<a href="<?php echo esc_url(home_url('aviation')); ?>">An aviation fan</a> - I attended <a href="https://airrace.org/" target="_blank">Reno Air Races</a> every September since 2008 and attended <a href="http://www.ajbs.fr/" target="_blank">Meeting Aerien de La Ferté-Alais</a> in France twice.</br>
 <a href="<?php echo esc_url(home_url('motorcycle')); ?>">A motorcycle rider</a> - I own two exciting Italian racers and used to participate in races a few times a year at <a href="https://en.wikipedia.org/wiki/Tsukuba_Circuit" target="_blank">Tsukuba Circuit</a>, and <a href="https://www.fsw.tv/en/" target="_blank">Fuji Speedway</a>.</br>
 <a href="http://www.youtube.com/watch?v=eJFo7AW0ys0" target="_blank">An accordion player</a> - I love playing <a href="https://en.wikipedia.org/wiki/Lars_Hollmer" target="_blank">Lars Hollmer</a>'s tunes with my Excelsior 1320. I used to enjoy talking with him when he came up to Tokyo.</p>
       <h2 class="fade-in">Hana</h2>
