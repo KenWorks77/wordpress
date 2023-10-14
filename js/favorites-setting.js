899492ca2599703e8d0035e2ef90c7d3f3e7a1e7
KenWorks77 <ken.yokochi.77@gmail.com>
2023-10-14 16:30:28 +0900

    お気に入り登録ボタンを押したら"added to favorites"になりdisabledする。

--- js/favorites-setting.js
+++ js/favorites-setting.js
@@ -2,35 +2,36 @@ let favePages = {};
 // onLoad
 document.addEventListener('DOMContentLoaded', () => {
   const showFaves = document.getElementById('faves');
+  const addBtnArr = document.querySelectorAll('.add-favorite');
   const expirationDate = new Date();
   expirationDate.setDate(expirationDate.getDate() + 30);
   if (document.cookie.includes('fave-pages')) {
-    getCookieData(showFaves, expirationDate);
+    getCookieData(showFaves, expirationDate, addBtnArr);
   }
-  editFavesBtn(showFaves, expirationDate);
-  addCookieData(showFaves, expirationDate);
+  editFavesBtn(showFaves, expirationDate, addBtnArr);
+  addCookieData(showFaves, expirationDate, addBtnArr);
 });
 
 // cookieを取得してjson形式に変換 → 結果をfavePagesに格納
-function getCookieData (showFaves, expirationDate) {
+function getCookieData (showFaves, expirationDate, addBtnArr) {
   let cookieArr = decodeURI(document.cookie).split('; ');
   let faves = cookieArr.filter((value) => {
     return value.includes('fave-pages');
   });
   let data = faves[0].split('=');
   favePages = JSON.parse(data[1]);
-  console.log('result: ', favePages);
   if (!showFaves.classList.contains('d-none')) {
-    displayFaves(showFaves, expirationDate);
+    displayFaves(showFaves, expirationDate, addBtnArr);
   }
+  checkBtnStatus(addBtnArr);
 }
 
 // edit favorites button
-function editFavesBtn (showFaves, expirationDate) {
+function editFavesBtn (showFaves, expirationDate, addBtnArr) {
   document.getElementById('btn-edit-faves').addEventListener('click', () => {
     if (showFaves.classList.contains('d-none')) {
       showFaves.classList.remove('d-none');
-      displayFaves(showFaves, expirationDate);
+      displayFaves(showFaves, expirationDate, addBtnArr);
     } else {
       showFaves.classList.add('d-none');
     }
@@ -38,7 +39,7 @@ function editFavesBtn (showFaves, expirationDate) {
 }
 
 // list表示
-function displayFaves (showFaves, expirationDate) {
+function displayFaves (showFaves, expirationDate, addBtnArr) {
   const ul = document.getElementById('faves-list');
   if (Object.keys(favePages).length) {
     let keys = Object.keys(favePages);
@@ -46,14 +47,14 @@ function displayFaves (showFaves, expirationDate) {
     keys.forEach((key) => {
       ul.innerHTML += '<li><a href="' + favePages[key].url + '"><p><img src="' + favePages[key].img + '">' + favePages[key].title + '</p></a> <button class="remove" data-slug="' + favePages[key].slug + '">×</button></li>';
     });
-    removeCookieData(showFaves, expirationDate);
+    removeCookieData(showFaves, expirationDate, addBtnArr);
   } else {
     ul.innerHTML = '<li class="default-li">No favorite pages</li>';
   }
 }
 
 // 削除ボタンを押して該当pageを削除
-function removeCookieData (showFaves, expirationDate) {
+function removeCookieData (showFaves, expirationDate, addBtnArr) {
   let removeBtns = document.querySelectorAll('.remove');
   removeBtns.forEach((button) => {
     button.addEventListener('click', () => {
@@ -62,14 +63,13 @@ function removeCookieData (showFaves, expirationDate) {
       cookieData = JSON.stringify(favePages);
       cookieData = encodeURI(cookieData.toString());
       document.cookie = 'fave-pages=' + cookieData + '; expires=' + expirationDate.toUTCString() + '; path=/wp/; Secure';
-      getCookieData(showFaves, expirationDate);
+      getCookieData(showFaves, expirationDate, addBtnArr);
     })
   });
 }
 
 // 追加ボタンを押してcookie登録
-function addCookieData (showFaves, expirationDate) {
-  const addBtnArr = document.querySelectorAll('.add-favorite');
+function addCookieData (showFaves, expirationDate, addBtnArr) {
   addBtnArr.forEach((targetBtn) => {
     targetBtn.addEventListener('click', () => {
       let cookieData = {};
@@ -85,7 +85,27 @@ function addCookieData (showFaves, expirationDate) {
       cookieData = JSON.stringify(cookieData);
       cookieData = encodeURI(cookieData.toString());
       document.cookie = 'fave-pages=' + cookieData + '; expires=' + expirationDate.toUTCString() + '; path=/wp/; Secure';
-      getCookieData(showFaves, expirationDate);
+      getCookieData(showFaves, expirationDate, addBtnArr);
     });
+  });
+}
+
+// check button status
+function checkBtnStatus(addBtnArr) {
+  addBtnArr.forEach((targetBtn) => {
+    if (Object.keys(favePages).length) {
+      let keysArr = Object.keys(favePages);
+      targetBtn.innerHTML = 'Add to favorites';
+      targetBtn.disabled = false;
+      keysArr.forEach((key) => {
+        if (key === targetBtn.dataset.slug) {
+          targetBtn.innerHTML = 'Added to favorites';
+          targetBtn.disabled = true;
+        }
+      });
+    } else {
+      targetBtn.innerHTML = 'Add to favorites';
+      targetBtn.disabled = false;
+    }
   });
 }
